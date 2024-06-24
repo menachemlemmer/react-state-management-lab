d56d4c858eb7aa269b0d8b110e1b2c9ec9ababaf
menachemlemmer <nmmlemmer@gmail.com>
2024-06-23 21:54:59 -0400

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,137 @@
+import { useState } from "react";
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [totalStrength, setTotalStrength] = useState(0);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      name: "Survivor",
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: "https://via.placeholder.com/150/92c952",
+    },
+    {
+      name: "Scavenger",
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: "https://via.placeholder.com/150/771796",
+    },
+    {
+      name: "Shadow",
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: "https://via.placeholder.com/150/24f355",
+    },
+    {
+      name: "Tracker",
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/d32776",
+    },
+    {
+      name: "Sharpshooter",
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: "https://via.placeholder.com/150/1ee8a4",
+    },
+    {
+      name: "Medic",
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: "https://via.placeholder.com/150/66b7d2",
+    },
+    {
+      name: "Engineer",
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: "https://via.placeholder.com/150/56acb2",
+    },
+    {
+      name: "Brawler",
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: "https://via.placeholder.com/150/8985dc",
+    },
+    {
+      name: "Infiltrator",
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: "https://via.placeholder.com/150/392537",
+    },
+    {
+      name: "Leader",
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/602b9e",
+    },
+  ]);
+
+  const handleAddFighter = (fighter) => {
+    if (money < fighter.price) {
+      console.log("Not enough money");
+      return;
+    } else {
+      setTeam([...team, fighter]);
+      setMoney(money - fighter.price);
+      setTotalStrength(totalStrength + fighter.strength);
+    }
+  };
+  return (
+    <>
+      <h1 className="px-4 py-8 text-4xl font-bold">Zombie Fighters</h1>
+      <h2 className="px-4 py-4 text-2xl font-bold">Money: {money}</h2>
+      <h2 className="px-4 py-4 text-2xl font-bold">Team</h2>
+      <ul className="flex flex-wrap justify-start">
+        {team.length === 0 && (
+          <p className="px-4 py-4">Pick some team members!</p>
+        )}
+        {team.map((fighter, index) => (
+          <li
+            className="mx-2 my-4 flex flex-col items-start gap-2 px-2 py-4"
+            key={index}
+          >
+            <img src={fighter.img} alt={fighter.name} />
+            <p>name: {fighter.name}</p>
+            <p>price: {fighter.price}</p>
+            <p>strength: {fighter.strength}</p>
+            <p>agility: {fighter.agility}</p>
+          </li>
+        ))}
+      </ul>
+      <h2 className="px-4 py-4 text-2xl font-bold">Zombie Fighters</h2>
+      <ul className="flex flex-wrap justify-start">
+        {zombieFighters.map((fighter, index) => (
+          <li
+            className="mx-2 my-4 flex flex-col items-start gap-2 px-2 py-4"
+            key={index}
+          >
+            <img src={fighter.img} alt={fighter.name} />
+            <p>name: {fighter.name}</p>
+            <p>price: {fighter.price}</p>
+            <p>strength: {fighter.strength}</p>
+            <p>agility: {fighter.agility}</p>
+            <button
+              className="rounded bg-black px-4 py-2 text-white"
+              onClick={() => handleAddFighter(fighter)}
+            >
+              Add
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default App;
